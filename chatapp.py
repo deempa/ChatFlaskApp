c049bea4bb19e2a91673832be99b49b661fb0f52
Lior Rabanian <masrab11@gmail.com>
2023-03-16 10:37:48 +0200

    default pattern of add message func

--- chatapp.py
+++ chatapp.py
@@ -1,10 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/<room_id>")
 def index(room_id):
     return render_template("index.html")
+
+app.route("/chat/<room_id>", methods=["GET", "POST"])
+def add_message(rood_id):
+    if request.method == "GET":
+        pass
+    else: # POST
+        pass
+    
 
 if __name__ == "__main__":
     app.run()
