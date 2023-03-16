8f558cb43588597c49f8dd0a35a8973d9f5bb24a
Lior Rabanian <masrab11@gmail.com>
2023-03-16 11:35:36 +0200

    feature done

--- chatapp.py
+++ chatapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, send_file
 from datetime import datetime
 
 app = Flask(__name__)
@@ -7,15 +7,16 @@ app = Flask(__name__)
 def index(room_id):
     return render_template("index.html")
 
-app.route("/chat/<room_id>", methods=["GET", "POST"])
-def add_message(room_id):
+@app.route("/api/chat/<room_id>", methods=["GET", "POST"])
+def chatroom(room_id):
     if request.method == "GET":
-        pass
+        return send_file(f"chats/chat_{room_id}.txt", mimetype='text/plain')
     else: # POST
         user_name = request.form["username"]
         message = request.form["msg"]
         date_time = get_datetime()
-        add_message_to_txt_file(room_id, user_name, message, datetime)
+        add_message_to_txt_file(room_id, user_name, message, date_time)
+        return redirect(url_for("chatroom"))
         
 def get_datetime():
     now = datetime.now()
@@ -31,6 +32,5 @@ def get_full_chat_messages(room_id):
         lines = f.readlines()
         return lines
     
-
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
