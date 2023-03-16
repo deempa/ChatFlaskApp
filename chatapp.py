8f371ce4f0f565a212cbd8a648f1c4537c31866a
Lior Rabanian <masrab11@gmail.com>
2023-03-16 10:47:20 +0200

    added new function that add message to txt file

--- chatapp.py
+++ chatapp.py
@@ -14,6 +14,8 @@ def add_message(rood_id):
     else: # POST
         user_name = request.form["username"]
         message = request.form["msg"]
+        date_time = get_datetime()
+        add_message_to_txt_file(room_id, )
         
 
 def get_datetime():
@@ -21,6 +23,9 @@ def get_datetime():
     date_string = now.strftime("%d/%m/%Y %H:%M:%S")
     return date_string
     
+def add_message_to_txt_file(room_id, user_name, message, date_time):
+    with open(f"chat_{room_id}.txt","a+") as f:
+        f.write(f"[{date_time}] {user_name}: {message}")
 
 if __name__ == "__main__":
     app.run()
