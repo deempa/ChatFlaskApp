3cf11cfd90db7c79bec7d297be72f861f5cccbc3
Lior Rabanian <masrab11@gmail.com>
2023-03-16 10:33:16 +0200

    basic render of index html in default root

--- chatapp.py
+++ chatapp.py
@@ -3,8 +3,8 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/<room_id>")
-def home(room_id):
-    return f"Youre in room {room_id}"
+def index(room_id):
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
